53137e2ac735085db2d33365c08fb51fec102543
Vedant Pandey <vedantpandey46@gmail.com>
2024-02-05 23:49:12 +0530

    Disable raw mode on exit

--- ladder.c
+++ ladder.c
@@ -1,12 +1,22 @@
+#include <stdlib.h>
+#include <sys/termios.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode(void) {
+    // Reset to original flags for current terminal session
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 // Disable canonical mode(aka Cooked Mode) in terminal
 void enableRawMode(void) {
-    struct termios raw;
-
     // Gets the attributes of running terminal in raw variable
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
 
     // c_lflag is for local flags
     // ECHO is a feature that echoes any key pressed to the terminal
