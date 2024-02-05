410723915f4f7046fc0a30d173b4acad20c22c5f
Vedant Pandey <vedantpandey46@gmail.com>
2024-02-05 23:54:40 +0530

    print ASCII for each character pressed

--- ladder.c
+++ ladder.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/termios.h>
 #include <termios.h>
@@ -31,6 +33,12 @@ int main(void) {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
     return 0;
 }
