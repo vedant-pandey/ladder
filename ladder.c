934ba25d933851b2c66ee636008542b62f5a71cc
Vedant Pandey <vedantpandey46@gmail.com>
2024-02-07 15:02:02 +0530

    Reset cursor on die

--- ladder.c
+++ ladder.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -83,12 +86,20 @@ void editorProcessKeypress(void) {
 
 /*** output ***/
 
-void editoryRefreshScreen() {
+void editorRefreshScreen(void) {
     // 4 describes to write 4 bytes in the terminal
     // \x1b initiates escape sequence and is 27 in decimal
     // [2J are other 3 bytes which are the escape sequence for clear screen
     // Ref https://vt100.net/docs/vt100-ug/chapter3.html#ED
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorDrawRows(void) {
+    int y;
+    for (y = 0; y < 24; ++y) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
 }
 
 /*** init ***/
@@ -97,8 +108,12 @@ int main(void) {
     enableRawMode();
 
     while (1) {
-        editoryRefreshScreen();
+        editorRefreshScreen();
         editorProcessKeypress();
+
+        editorDrawRows();
+
+        write(STDOUT_FILENO, "\x1b[H", 3);
     }
     return 0;
 }
